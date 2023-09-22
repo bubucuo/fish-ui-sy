0d8ef3f5c5739e78c570ca906a28cdbb155b3a69
bubucuo <276185876@qq.com>
2023-09-22 16:54:40 +0800

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
+  rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
