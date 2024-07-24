acfb758dd6b043da5b6522e9a4fa711c36468967
bubucuo <276185876@qq.com>
2024-07-24 23:12:56 +0800

    save spacing shadows  strkeWidth mdx

---
+++ src/theme/strokeWidths/index.stories.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { teamsLightTheme } from "@/index";
+import type { StrokeWidthTokens } from "@/index";
+
+const theme = teamsLightTheme;
+
+export const StrokeWidths = () => {
+  const strokeWidthTokens = Object.keys(theme).filter((tokenName) =>
+    tokenName.startsWith("strokeWidth")
+  ) as (keyof StrokeWidthTokens)[];
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "auto 1fr",
+        gap: "10px",
+        alignItems: "center",
+      }}
+    >
+      {strokeWidthTokens.map((strokeWidth: keyof StrokeWidthTokens) => [
+        <div key={strokeWidth}>{strokeWidth}</div>,
+        <div
+          key={`${strokeWidth}-value`}
+          style={{ borderBottom: `${theme[strokeWidth]} solid black` }}
+        />,
+      ])}
+    </div>
+  );
+};
+
+export default {
+  title: "主题/StrokeWidths",
+};
