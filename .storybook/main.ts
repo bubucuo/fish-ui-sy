118c7ed8895f51fb7bd562a5028be2d5a9ad00f0
bubucuo <276185876@qq.com>
2024-07-24 21:55:35 +0800

    支持mdx

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,28 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import remarkGfm from "remark-gfm";
 
 const config: StorybookConfig = {
   stories: [
     "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)", //
+    "../src/**/**/*.mdx",
+    "../src/**/index.stories.@(js|jsx|mjs|ts|tsx|mdx)",
+    "../src/**/**/index.stories.@(js|jsx|mjs|ts|tsx|mdx)",
   ],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        mdxPluginOptions: {
+          mdxCompileOptions: {
+            remarkPlugins: [remarkGfm],
+          },
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/react-vite",
@@ -20,8 +32,5 @@ const config: StorybookConfig = {
     //👇 See the table below for the list of supported options
     defaultName: "Documentation",
   },
-  // 设置stories没有export default
-
-  //
 };
 export default config;
