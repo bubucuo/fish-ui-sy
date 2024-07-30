f195fdf911f540ee507b2c0a1cdc2f126765e85b
bubucuo <276185876@qq.com>
2024-07-30 14:57:52 +0800

    save ls4. css方案选择

--- src/components/Button/stories/Custom.stories.tsx
+++ src/components/Button/stories/Custom.stories.tsx
@@ -1,6 +1,18 @@
-export const Custom = {
-  args: {
-    primary: true,
-    label: "Custom",
+import { Button } from "fish-ui-sy";
+import { makeStyles } from "@griffel/react";
+
+const useClasses = makeStyles({
+  root: {
+    backgroundColor: "red",
+    padding: "100px",
   },
+});
+
+export const Custom = () => {
+  const styles = useClasses();
+  return (
+    <div className={styles.root}>
+      <Button primary label="Custom Button" />
+    </div>
+  );
 };
