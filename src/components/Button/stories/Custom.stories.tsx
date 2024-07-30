9331d7103d65e1250993c3630a1b511363318991
bubucuo <276185876@qq.com>
2024-07-30 17:45:35 +0800

    save 6. 组件库的主题与tokens

--- src/components/Button/stories/Custom.stories.tsx
+++ src/components/Button/stories/Custom.stories.tsx
@@ -1,10 +1,12 @@
-import { Button } from "fish-ui-sy";
+import { Button, tokens } from "fish-ui-sy";
 import { makeStyles } from "@griffel/react";
 
 const useClasses = makeStyles({
   root: {
     backgroundColor: "red",
     padding: "100px",
+    // border: `10px solid pink`,
+    border: `10px solid ${tokens.colorPaletteRedBackground1}`,
   },
 });
 
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from "./tokens";
 export * from "./components/Button";
 export * from "./components/Header";
 export * from "./components/Page";
