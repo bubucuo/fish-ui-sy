3467fa1e15450c9b81dea17ae4047c5f93d135f1
bubucuo <276185876@qq.com>
2024-07-30 10:54:19 +0800

    save ls2

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./Button";
-export * from "./Header";
-export * from "./Page";
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from "./components/Button";
+export * from "./components/Header";
+export * from "./components/Page";
