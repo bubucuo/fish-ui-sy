b99d8ef933b5acc0ac111253bf83c3dd8568091d
bubucuo <276185876@qq.com>
2024-08-01 15:04:00 +0800

    12. build 组件库

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import dts from "vite-plugin-dts";
+
+function resolve(str: string) {
+  return path.resolve(__dirname, str);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true })],
   resolve: {
     alias: {
       "fish-ui-sy": path.resolve(__dirname, "./src/index.ts"),
@@ -14,5 +19,25 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+  },
+
+  build: {
+    outDir: "dist",
+    lib: {
+      entry: resolve("src/index.ts"),
+      name: "fish-ui-sy",
+      fileName: (format) => `fish-ui-sy.${format}.js`,
+      formats: ["cjs", "es", "umd"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+        format: "cjs",
+      },
+    },
   },
 });
